f37b4e481684265ecc0ed00d59c1bfa9c9f875ef
xeeny <xeeny@wp.pl>
2019-06-11 23:20:49 +0200

    implenet base

---
+++ plashcards.py
@@ -0,0 +1,42 @@
+"""plashcards.
+
+Usage:
+  plashcards.py new <file>
+  plashcards.py test <deck>
+
+Options:
+  -h --help     Show this screen.
+
+"""
+import json
+import os
+import random
+from docopt import docopt
+
+def makeDeck(fname):
+    deck = []
+    lines = open(fname, "r").read().split("\n")
+    for line in lines[:-1]:
+        sline = line.split("\t")
+        deck.append({
+            "front" : sline[0],
+            "back"  : sline[1]
+        })
+    f, ext = os.path.splitext(fname)
+    open(f + ".pdeck", "w").write(json.dumps(deck, sort_keys=True, indent=4))
+
+def test(fname):
+    deck = json.loads(open(fname, "r").read())
+    random.shuffle(deck)
+    for card in deck:
+        print("front:", card["front"])
+        input()
+        print("back: ", card["back"])
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Naval Fate 2.0')
+    print(arguments)
+    if arguments["new"]:
+        makeDeck(arguments["<file>"])
+    if arguments["test"]:
+        test(arguments["<deck>"])
